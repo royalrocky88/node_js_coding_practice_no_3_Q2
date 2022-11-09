1a8f2c56f95eda6e9be1e10a947934ab5b68a62a
Rocky Ranjan <royalrocky88@gmail.com>
2022-11-09 16:04:27 +0000

    Gadgets Page

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/gadgets", (request, response) => {
+  response.sendFile("./gadgets.html", { root: __dirname });
+});
+
+app.listen(3000);
+
+module.exports = app;
